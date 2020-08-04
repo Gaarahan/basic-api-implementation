b0dd700be4edb66b0e0890f989c56cb2533f34f3
gaarahan <gaarahan@foxmail.com>
2020-08-04 21:16:36 +0800

    [Gaarahan] complete test of "add new rsEvent"

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -67,6 +69,31 @@ class RsListApplicationTests {
         .andExpect(jsonPath("$[0].key", is("key")))
         .andExpect(jsonPath("$[1].key", is("key")))
         .andExpect(jsonPath("$[2].key", is("key")))
+
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void should_add_new_rs_event() throws Exception {
+    String newRsStr = "{ \"eventName\": \"rs-new\", \"key\": \"new\" }";
+    this.mockMvc.perform(
+        post("/rs/add").content(newRsStr)
+            .contentType(MediaType.APPLICATION_JSON)
+    )
+        .andExpect(status().isOk());
+
+    this.mockMvc.perform(get("/rs/list"))
+        .andExpect(jsonPath("$", hasSize(4)))
+
+        .andExpect(jsonPath("$[0].eventName", is("rs1")))
+        .andExpect(jsonPath("$[1].eventName", is("rs2")))
+        .andExpect(jsonPath("$[2].eventName", is("rs3")))
+        .andExpect(jsonPath("$[3].eventName", is("rs-new")))
+
+        .andExpect(jsonPath("$[0].key", is("key")))
+        .andExpect(jsonPath("$[1].key", is("key")))
+        .andExpect(jsonPath("$[2].key", is("key")))
+        .andExpect(jsonPath("$[3].key", is("new")))
 
         .andExpect(status().isOk());
   }
