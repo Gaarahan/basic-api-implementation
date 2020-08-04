a36da21c2c9c43c92d7f7a70f4d483c18e242b1e
gaarahan <gaarahan@foxmail.com>
2020-08-04 20:25:52 +0800

    [Gaarahan] resolve the get-list request

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,28 @@
 package com.thoughtworks.rslist.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList;
+
+  public RsController() {
+    this.rsList = new ArrayList<>();
+    this.rsList.add(new RsEvent("rs1", "key"));
+    this.rsList.add(new RsEvent("rs2", "key"));
+    this.rsList.add(new RsEvent("rs3", "key"));
+  }
+
+  @GetMapping("rs/list")
+  public List<RsEvent> getSplitOrAllRsList (@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end) {
+    if (start != null && end != null) {
+      return this.rsList.subList(start - 1, end);
+    }
+    return this.rsList;
+  }
 }
