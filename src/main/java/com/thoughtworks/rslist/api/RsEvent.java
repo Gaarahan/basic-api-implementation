f98c69ac1ca20d656583d6ef1a01e39a99d80e5b
gaarahan <gaarahan@foxmail.com>
2020-08-05 15:25:30 +0800

    [Gaarahan] could customize statusCode and body

--- src/main/java/com/thoughtworks/rslist/api/RsEvent.java
+++ src/main/java/com/thoughtworks/rslist/api/RsEvent.java
@@ -1,13 +1,18 @@
 package com.thoughtworks.rslist.api;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class RsEvent {
 
   private String eventName;
   private String key;
+  private User user;
 
-  public RsEvent(String eventName, String key) {
+  public RsEvent(String eventName, String key, User user) {
     this.eventName = eventName;
     this.key = key;
+    this.user = user;
   }
 
   public String getEventName() {
@@ -16,6 +21,16 @@ public class RsEvent {
 
   public String getKey() {
     return key;
+  }
+
+  @JsonIgnore
+  public User getUser() {
+    return user;
+  }
+
+  @JsonProperty
+  public void setUser(User user) {
+    this.user = user;
   }
 
   public void setEventName(String eventName) {
