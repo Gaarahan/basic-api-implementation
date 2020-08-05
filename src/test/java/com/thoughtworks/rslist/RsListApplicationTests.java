f43254f7b1c3978d66b6c0ef294ae606695d31ef
gaarahan <gaarahan@foxmail.com>
2020-08-05 22:38:11 +0800

    [Gaarahan] add test: validation

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,37 +1,43 @@
 package com.thoughtworks.rslist;
 
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.rslist.api.RsController;
-import com.thoughtworks.rslist.api.RsEvent;
-import com.thoughtworks.rslist.api.User;
+import com.thoughtworks.rslist.domain.RsEvent;
+import com.thoughtworks.rslist.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class RsListApplicationTests {
 
   private MockMvc mockMvc;
+  private User curUser;
+  private RsEvent rsEvent;
+  private ObjectMapper ignoreAnnotationsMapper;
 
   @BeforeEach
   private void setup () {
     this.mockMvc = MockMvcBuilders.standaloneSetup(new RsController()).build();
+    this.curUser = new User("han", 21, "male", "gaarahan@foxmail.com", "13755556666");
+    this.rsEvent = new RsEvent("rs-new", "key", curUser);
+
+    this.ignoreAnnotationsMapper = new ObjectMapper();
+    ignoreAnnotationsMapper.configure(MapperFeature.USE_ANNOTATIONS, false);
   }
 
   private ArrayList<RsEvent> getCurrentRsList () throws Exception {
@@ -105,7 +111,6 @@ class RsListApplicationTests {
 
   @Test
   void should_add_new_rs_event() throws Exception {
-    User curUser = new User("han", 21, "male", "gaarahan@foxmail.com", "12455556666");
     RsEvent rsEvent = new RsEvent("rs-new", "new", curUser);
     String newRsEventStr = new ObjectMapper().writeValueAsString(rsEvent);
 
@@ -157,5 +162,72 @@ class RsListApplicationTests {
 
     ArrayList<RsEvent> actualList = this.getCurrentRsList();
     Assertions.assertEquals(expectList, actualList);
+  }
+
+  @Test
+  void should_got_bad_request_message_when_event_name_is_null() throws Exception {
+    rsEvent.setEventName(null);
+    this.mockMvc.perform(post("/rs/add").content(ignoreAnnotationsMapper.writeValueAsString(rsEvent)).contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void should_got_bad_request_message_when_key_is_null() throws Exception {
+    rsEvent.setKey(null);
+    this.mockMvc.perform(
+        post("/rs/add").content(ignoreAnnotationsMapper.writeValueAsString(rsEvent)).contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+    )
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void should_got_bad_request_message_when_user_is_null() throws Exception {
+    RsEvent newRsEvent = new RsEvent("rs-new", "key", null);
+    this.mockMvc.perform(
+        post("/rs/add")
+            .content(ignoreAnnotationsMapper.writeValueAsString(newRsEvent))
+            .contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+    )
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void should_got_bad_request_message_when_user_name_has_error() throws Exception {
+    curUser.setName("123456789");
+    RsEvent newRsEvent = new RsEvent("rs-new", "key", curUser);
+    this.mockMvc.perform(post("/rs/add").content(ignoreAnnotationsMapper.writeValueAsString(newRsEvent)).contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void should_got_bad_request_message_when_user_age_has_error() throws Exception {
+    curUser.setAge(10);
+    RsEvent newRsEvent = new RsEvent("rs-new", "key", curUser);
+    this.mockMvc.perform(post("/rs/add").content(ignoreAnnotationsMapper.writeValueAsString(newRsEvent)).contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void should_got_bad_request_message_when_user_gender_has_error() throws Exception {
+    curUser.setGender(null);
+    RsEvent newRsEvent = new RsEvent("rs-new", "key", curUser);
+    this.mockMvc.perform(post("/rs/add").content(ignoreAnnotationsMapper.writeValueAsString(newRsEvent)).contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void should_got_bad_request_message_when_user_email_has_error() throws Exception {
+    curUser.setEmail("aaaaaaaaa");
+    RsEvent newRsEvent = new RsEvent("rs-new", "key", curUser);
+    this.mockMvc.perform(post("/rs/add").content(ignoreAnnotationsMapper.writeValueAsString(newRsEvent)).contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void should_got_bad_request_message_when_user_phone_has_error() throws Exception {
+    curUser.setPhone("11111");
+    RsEvent newRsEvent = new RsEvent("rs-new", "key", curUser);
+    this.mockMvc.perform(post("/rs/add").content(ignoreAnnotationsMapper.writeValueAsString(newRsEvent)).contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest());
   }
 }
